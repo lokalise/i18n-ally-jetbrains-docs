ec8e1e9446f9b07637fdbd4171be4f154faee454
Alex Terehov <terales@users.noreply.github.com>
2021-12-15 13:50:34 +0200

    IDE-1067 Fix favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
   organizationName: 'lokalise', // Usually your GitHub org/user name.
   projectName: 'i18nally', // Usually your repo name.
 
